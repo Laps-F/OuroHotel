e333ed3d4a85b5121a1750f2466ea28166183702
Laps-F <maurolaps@gmail.com>
2023-06-23 19:13:22 -0300

    Lista de Cards e Fetch Data do Banco de Dados

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+import { addDoc, collection, getDocs, getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { useEffect, useState } from 'react';
+
+import Card from "./Components/Card";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBOSSUxqm3Hqh-aXSmUTOLwDS_2Rimx1Zc",
+  authDomain: "ourohotel-df08d.firebaseapp.com",
+  projectId: "ourohotel-df08d",
+  storageBucket: "ourohotel-df08d.appspot.com",
+  messagingSenderId: "882292019392",
+  appId: "1:882292019392:web:099e13532f50bb77479d25"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [users, setUsers] = useState([]);
+  const [hospedagens, setHospedagens] = useState([]);
+
+  const db = getFirestore(app);
+  const userCollectionRef = collection(db, "users");
+
+  const hospedagensCollection = collection(db, "hospedagens");
+
+  useEffect(() => {
+    const getUsers = async () => {
+      var data = await getDocs(userCollectionRef);
+      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+    }
+    
+    getUsers();
+  }, [userCollectionRef]);
+
+  useEffect(() => {
+    const getHospedagens = async () => {
+      var data = await getDocs(hospedagensCollection);
+      setHospedagens(data.docs.map(doc => ({...doc.data(), id: doc.id})));
+    }
+
+    getHospedagens();
+  }, [hospedagensCollection]);
+
+  function createUser(){
+    addDoc(userCollectionRef, {name, email});
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <input type="text" placeholder="Name..." value={name} onChange={(e) => setName(e.target.value)}/>
+      <input type="text" placeholder="Email..." value={email} onChange={(e) => setEmail(e.target.value)}/>
+      <button onClick={createUser}>Create User</button> */}
+      {
+        hospedagens.map((hospedagem) => {
+          return (
+            <Card 
+              nome={hospedagem.Hotel} 
+              endereco={hospedagem.Endereco} 
+              preco={hospedagem.PrecoDiaria} 
+              qtdcamas={hospedagem.QtdCamas} 
+              tipocama={hospedagem.TipoCamas}
+              foto={hospedagem.Foto}
+            />
+          );
+        })
+      }
+      
     </div>
   );
 }
