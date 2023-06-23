d76b494d5e386533d76293281ef19dbeb16da50b
Laps-F <maurolaps@gmail.com>
2023-06-23 19:50:41 -0300

    Reorganizando codigo

--- src/App.js
+++ src/App.js
@@ -1,59 +1,33 @@
 /* eslint-disable no-unused-vars */
 
 import React from 'react';
-import { addDoc, collection, getDocs, getFirestore } from "firebase/firestore";
-import { initializeApp } from "firebase/app";
+
 import { useEffect, useState } from 'react';
+import { collection, getDocs } from "firebase/firestore";
 
-import Modal from './Components/Modal';
-import Register from './Components/Register';
-import Login from './Components/Login';
-import Card from "./Components/Card";
+import { DB } from "./constants/Database";
 
+import Modal from './components/Modal';
+import Register from './components/Register';
+import Login from './components/Login';
+import Card from "./components/Card";
 import './App.css';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBOSSUxqm3Hqh-aXSmUTOLwDS_2Rimx1Zc",
-  authDomain: "ourohotel-df08d.firebaseapp.com",
-  projectId: "ourohotel-df08d",
-  storageBucket: "ourohotel-df08d.appspot.com",
-  messagingSenderId: "882292019392",
-  appId: "1:882292019392:web:099e13532f50bb77479d25"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 function App() {
   const [openModal, setOpenModal] = useState(false);
   const [currForm, setCurrentForm] = useState('register');
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [users, setUsers] = useState([]);
   const [hospedagens, setHospedagens] = useState([]);
 
-  const db = getFirestore(app);
-  const userCollectionRef = collection(db, "users");
-
-  const hospedagensCollection = collection(db, "hospedagens");
-
-  useEffect(() => {
-    const getUsers = async () => {
-      var data = await getDocs(userCollectionRef);
-      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
-    }
-    
-    getUsers();
-  }, [userCollectionRef]);
+  // const hospendagensCollection = collection(DB, "hospedagens");
 
   useEffect(() => {
     const getHospedagens = async () => {
-      var data = await getDocs(hospedagensCollection);
-      setHospedagens(data.docs.map(doc => ({...doc.data(), id: doc.id})));
+      // const data = await getDocs(hospendagensCollection);
+      // setHospedagens(data.docs.map(doc => ({...doc.data(), id: doc.id})));
     }
 
     getHospedagens();
-  }, [hospedagensCollection]);
+  }, []);
 
   const toggleForm = (formName) => {
     setCurrentForm(formName);
@@ -61,10 +35,14 @@ function App() {
 
   function opModal() {
     setOpenModal(!openModal);
+    if(currForm === 'login')
+      setCurrentForm('register');
+    else
+      setCurrentForm('login');
   }
   
-  function createUser(){
-    addDoc(userCollectionRef, {name, email});
+  function closeModal(){
+    setOpenModal(!openModal);
   }
 
   return (
@@ -75,17 +53,13 @@ function App() {
           Cadastro
         </button>
 
-        {/* <Modal isOpen={openModal} setClose={()=>{setOpenModal(!openModal)}}> */}
         <Modal isOpen={openModal} setClose={opModal}>
           {
-            currForm === 'register' ?  <Register onFormSwitch={toggleForm} closeAfter={opModal}/> : <Login />
+            currForm === 'register' ?  <Register onFormSwitch={toggleForm} closeAfter={closeModal}/> : <Login />
           }
         </Modal>
 
       </header>
-      {/* <input type="text" placeholder="Name..." value={name} onChange={(e) => setName(e.target.value)}/>
-      <input type="text" placeholder="Email..." value={email} onChange={(e) => setEmail(e.target.value)}/>
-      <button onClick={createUser}>Create User</button> */}
       {
         hospedagens.map((hospedagem) => {
           return (
