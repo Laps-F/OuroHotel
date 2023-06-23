d8fecdcb7e0ed36d6832994dc7ac3e5604df34d4
Arthur Henrique Santos Celestino <arthur.celestino@aluno.ufop,edu.br>
2023-06-23 11:14:33 -0300

    Fazendo as validações

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,35 +1,118 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './styles.css';
 
-const Register = (prop) => {
+const Register = ({onFormSwitch}) => {
   const [email, setEmail] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [conf_password, setConfPassword] = useState('');  
+  const [conf_password, setConfPassword] = useState('');
 
-  const handleSubmit = (e) => {
+  const [errorConf, setErrorConf] = useState(false);
+  const [errorPassDif, setErrorPassDif] = useState(false);
+  const [errorName, setErrorName] = useState(false);
+  const [errorEmail, setErrorEmail] = useState(false);
+  const [errorPassConf, setErrorPassConf] = useState(false);
+  
+  const[handleOpen, setHandleOpen] = useState(false);
+
+  useEffect( () => {
+    // if(username.length === 0) {
+    //   setErrorName(true);
+    //   console.log("Please enter");
+    // }
+    // if (password !== conf_password)
+    //   setErrorPassDif(true);
+    
+
+  }, [username, password, email, conf_password])
+
+  useEffect(() => {
+    setHandleOpen(false);
+  }, []);
+
+  function updateUsername(e){
+    setUsername(e.target.value);
+    setErrorName(false);
+  }
+
+ function handleSubmit (e) {
+    setHandleOpen(true);
     e.preventDefault();
-    console.log(email);
+
+    if(username.length === 0)
+      setErrorName(true);
+    if(email.length === 0)
+      setErrorEmail(true);
+    if(password.length === 0)
+      setErrorPassConf(true);
+    if(conf_password.length === 0)
+      setErrorConf(true);
+    if(password !== conf_password)
+      setErrorPassDif(true);
+
+
+    if(username.length === 0 | password.length === 0 | email.length === 0 | conf_password.length === 0 | password !== conf_password)
+      return ;
+    
+    alert("Usuário cadatrado com sucesso!");
+  }
+
+  function kl(e){
+    console.log("PASS",password);
+    console.log("conf",e.target.value);
+    if(password === e.target.value)
+      setErrorPassDif(false);
   }
 
   return (
     <div className='global'>
-        <form className='regform' onSubmit={handleSubmit}>
+        <>
             <label htmlFor="username">Username</label>
-            <input value={username} onChange={(e) => [setUsername(e.target.value)]} type="username" placeholder="Username" id='username' name='username'/>
-            
+            <input 
+              value={username} 
+              // onChange={(e) => {setUsername(e.target.value); setErrorName(false)}}
+              onChange={updateUsername}
+              type="username" 
+              placeholder="Username" 
+              id='username' 
+              name='username'
+            />
+            {errorName & handleOpen === true ? <a>error</a> : <a></a>}
+
             <label htmlFor="email">Email</label>
-            <input value={email} onChange={(e) => [setEmail(e.target.value)]} type="email" placeholder="youremail@example.com" id='email' name='email'/>
+            <input value={email} 
+              onChange={(e) => {setEmail(e.target.value); setErrorEmail(false)}}  
+              type="email" 
+              placeholder="youremail@example.com" 
+              id='email' 
+              name='email'
+            />
+
 
             <label htmlFor="password">Password</label>
-            <input value={password} onChange={(e) => [setPassword(e.target.value)]} type="password" placeholder="Password" id='password' name='password'/>
+            <input value={password} 
+              onChange={(e) => {setPassword(e.target.value); setErrorPassConf(false)}}  
+              type="password" 
+              placeholder="Password" 
+              id='password' 
+              name='password'
+            />
 
             <label htmlFor="passwordConfirm">Confirm Password</label>
-            <input value={conf_password} onChange={(e) => [setConfPassword(e.target.value)]} type="password_conf" placeholder="Confirm Password" id='password_conf' name='password_conf'/>
+            <input value={conf_password} 
+              onChange={(e) => {setConfPassword(e.target.value); setErrorConf(false); kl(e)}} 
+              type="password_conf" 
+              placeholder="Confirm Password" 
+              id='password_conf' 
+              name='password_conf'
+            />
 
-            <button type='register'>Registre-se</button>
-        </form>
-        <button className='alreadyacc-button'onClick={()=>prop.onFormSwitch('login')}>Já tem uma conta? Faça Login</button>
+            <button type='register' onClick={handleSubmit}>Registre-se</button>
+            {errorPassDif & handleOpen === true ? <a>error</a> : <a></a>}
+        </>
+        <button className='alreadyacc-button'
+          onClick={()=>onFormSwitch('login')}>Já tem uma conta? Faça Login
+        </button>
     </div>
   )
 }
