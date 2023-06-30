e002e73f125662d7d726730cb2cf9edf8b8f3fd4
Laps-F <maurolaps@gmail.com>
2023-06-30 20:46:47 -0300

    Estilizando cards da lista

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -6,18 +6,19 @@ function Card({nome, endereco, preco, qtdcamas, tipocama, foto}) {
     return (
         <div className="card">
             <div className="image-container">
-                <img src={foto} alt="Foto" width="250"/>
+                <img src={foto} alt="Foto" width="250" className="imagem"/>
             </div>
-            <div className="title-container">
-                <h1 className="text">Hotel: {nome}</h1>
-            </div>
-            <div className="info-container">
-                <p className="text">Endereço: {endereco}</p>
-                <p className="text">Número de Camas: {qtdcamas}</p>
-                <p className="text">Tipo das Camas: {tipocama}</p>
+            <div>
+                <div className="title-container">
+                    <h1 className="text">{nome}</h1>
+                </div>
+                <div className="info-container">
+                    <p className="text info">{endereco}</p>
+                    <p className="text info">{qtdcamas} cama(s) de {tipocama}</p>
+                </div>
             </div>
             <div className="price-container">
-                <p className="text">Preço: R$ {preco}</p>
+                <p className="text">R$ {preco}</p>
             </div>
         </div>
     );
