88a0660425ea2b833a3d5849a7e0946a2d451f26
Laps-F <maurolaps@gmail.com>
2023-07-08 01:23:01 -0300

    Escondendo a scrollbar

--- src/components/CardList/index.jsx
+++ src/components/CardList/index.jsx
@@ -2,20 +2,24 @@ import Card from "../Card";
 
 import "./style.css";
 
-function CardList({hospedagens}) {
+function CardList({hospedagens, reservar}) {
     return (
       <div className="list">
         {hospedagens.map((hospedagem) => {
           return (
-              <Card 
-                key={hospedagem.id}
-                nome={hospedagem.Hotel} 
-                endereco={hospedagem.Endereco} 
-                preco={hospedagem.PrecoDiaria} 
-                qtdcamas={hospedagem.QtdCamas} 
-                tipocama={hospedagem.TipoCamas}
-                foto={hospedagem.Foto}
-              />
+            hospedagem.Reservas.some((reserva) => reserva.reservado === false) ?
+            <Card 
+              key={hospedagem.id}
+              id={hospedagem.id}
+              nome={hospedagem.Hotel} 
+              endereco={hospedagem.Endereco} 
+              preco={hospedagem.PrecoDiaria} 
+              qtdcamas={hospedagem.QtdCamas} 
+              tipocama={hospedagem.TipoCamas}
+              foto={hospedagem.Foto}
+              datas={hospedagem.Datas}
+              reservar={reservar}
+            /> : <div></div>
           );
         })}
       </div>
