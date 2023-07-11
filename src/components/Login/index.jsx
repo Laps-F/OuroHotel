2a86858d5aef4bc51e032ef038148a5e1be0566d
Laps-F <maurolaps@gmail.com>
2023-07-11 17:59:35 -0300

    Adicionando erro de email e senha errados

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -5,9 +5,10 @@ import "./styles.css";
 const Login = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
-    const[handleOpen, setHandleOpen] = useState(false);
+    const [handleOpen, setHandleOpen] = useState(false);
     const [errorEmail, setErrorEmail] = useState(false);
     const [errorPassConf, setErrorPassConf] = useState(false); 
+    const [errorLogin, setErrorLogin] = useState(false);
 
     useEffect(() => {
       setHandleOpen(false);
@@ -17,13 +18,6 @@ const Login = (props) => {
         setHandleOpen(true);
         e.preventDefault();
         
-        props.users.map((user) => {
-          if(email === user.email && pass === user.password){
-            props.closeAfter();
-            props.loginHandle(email);
-          }
-        })
-
         if(email.length === 0){
           setErrorEmail(true);
         }
@@ -31,6 +25,23 @@ const Login = (props) => {
           setErrorPassConf(true);
     
         if(pass.length === 0 | email.length === 0)
+          return ;
+        
+        var verif = 0;
+        props.users.map((user) => {
+          if(email === user.email && pass === user.password)
+            verif = 1;
+        })
+
+        if(verif === 1){
+          props.closeAfter();
+          props.loginHandle(email);
+        }
+        else {
+          setErrorLogin(true);
+        }
+
+        if(verif === 0)
           return ;
     }
 
@@ -63,6 +74,7 @@ const Login = (props) => {
                 />
                 {errorPassConf & handleOpen === true ? <a>É necessário digitar uma senha</a> :<a></a>}
                 <button type="submit"> Entrar </button>
+                {errorLogin & handleOpen === true ? <a>Email ou Senha incorretos</a> :<a></a>}
             </form>
             <button className='link-btn' 
               onClick={() => props.onFormSwitch('register')}>Não possui uma conta? Crie uma aqui
