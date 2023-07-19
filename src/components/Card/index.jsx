4d51a1f7077b3204e9cc12b46949a24d5ee26251
Arthur Henrique Santos Celestino <arthur.celestino@aluno.ufop,edu.br>
2023-07-19 00:40:38 -0300

    colocando calendario funcional no modal de confirmação

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -19,6 +19,7 @@ function Card({
 
     const [disabled, setDisabled] = useState(false);
     const [openModal, setOpenModal] = useState(false);
+    const [fDatas, setfDatas] = useState([]);
 
     useEffect(() => {
         const value = vagas.filter((vaga) => { return vaga.reservado === true}).length;
@@ -29,16 +30,48 @@ function Card({
             setDisabled(true);
     }, [vagas]);
 
-    async function handleReserva(){
-        var value = vagas.filter((vaga) => { return vaga.reservado === true}).length;
-        if(value < datas.length){
-            setDisabled(false);
-            await reservar(id, datas[0]);
-            alert("Reserva feita");
+    useEffect(() => {
+        formatData();
+    }, []);
+
+    function formatData(){
+        var newDate = [];
+        datas.map(async (data) => {
+            const fdata = `${data.toDate().getDate()}/${data.toDate().getMonth() + 1}/${data.toDate().getFullYear()}`;
+            newDate = [...newDate, fdata];
+        })
+        //const filtered = newDate.filter((item, index) => newDate.indexOf(item) === index);
+        const filtered = [...new Set(newDate)];
+        // const availableDatas = filtered.filter(item => vagas.map((vaga) => {
+        //     const tempdata = `${vaga.data.data.toDate().getDate()}/${vaga.data.data.toDate().getMonth() + 1}/${vaga.data.data.toDate().getFullYear()}`;
+        //     console.log(tempdata);
+        //     return tempdata === item;
+        // }));
+        var availableDatas = [];
+        for(var i = 0; i < filtered.length; i++) {
+            var tempfiltered = filtered[i];
+            for(var j = 0; j < vagas.length; j++) {
+                var tempdata = `${vagas[j].data.data.toDate().getDate()}/${vagas[j].data.data.toDate().getMonth() + 1}/${vagas[j].data.data.toDate().getFullYear()}`
+                if (tempfiltered === tempdata){
+                    availableDatas = [...availableDatas, tempdata];
+                }
+            }
         }
-        else
-            setDisabled(true);
+        const newArray = filtered.filter(item => !availableDatas.includes(item));
+        setfDatas(newArray);
     }
+
+    // async function handleReserva(){
+    //     var value = vagas.filter((vaga) => { return vaga.reservado === true}).length;
+    //     if(value < datas.length){
+    //         setDisabled(false);
+    //         // await reservar(id, datas[0]);
+    //         // alert("Reserva feita");
+    //     }
+    //     else
+    //         setDisabled(true);
+    // }
+
     return (
         <div className="card">
             <div className="image-container">
@@ -49,9 +82,13 @@ function Card({
                     <h1 className="text">{nome}</h1>
                 </div>
                 <div className="info-container">
-                    {datas.map((data) => {
+                    {/* {datas.map((data) => {
                         const fdata = `${data.toDate().getDate()}/${data.toDate().getMonth() + 1}/${data.toDate().getFullYear()}`;
+                        const newDate = [...fDatas, fdata];
                         return <p key={fdata} className='text info'>{fdata}</p>;
+                    })} */}
+                    {fDatas.map((fDatas) => {
+                        return <p key={fDatas} className='text info'>{fDatas}</p>;
                     })}
                     <p className="text info">{endereco}</p>
                     <p className="text info">{qtdcamas} cama(s) de {tipocama}</p>
@@ -71,7 +108,7 @@ function Card({
                 </div>
             </div>
             <Modal isOpen={openModal}>
-                <Confirmation></Confirmation>
+                <Confirmation datadb={fDatas} reserva={reservar} id={id}></Confirmation>
             </Modal>
         </div>
     );
--- src/components/Confirmation/index.jsx
+++ src/components/Confirmation/index.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import TextField from '@mui/material/TextField';
 
-const Confirmation = () => {
+const Confirmation = ({datadb, reserva, id}) => {
+    const [selectedDate, setSelectedDate] = useState(null);
+    // const allowedDates = [
+    //     new Date(2023, 6, 1), // 1 de julho de 2023
+    //     new Date(2023, 6, 15), // 15 de julho de 2023
+    //     new Date(2023, 6, 30) // 30 de julho de 2023
+    // ];
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    };
+      
+    async function confirmReserva () {
+        await reserva(id, selectedDate);
+        alert("cu");
+    };
   return (
-    <div>Confirmation</div>
+    <div>
+        <DatePicker
+            selected={selectedDate}
+            onChange={handleDateChange}
+            filterDate={(date) => datadb.some((datadb) => (`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`).toString() === datadb)}
+            dateFormat="dd/MM/yyyy"
+            placeholderText="Selecione uma data"
+            customInput={<TextField />}
+        />
+        <button
+            onClick={confirmReserva}
+        >
+            Confirmar
+        </button>
+
+    </div>
   )
 }
 
